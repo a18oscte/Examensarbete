57a1f0f17d230642ff4f293c292abcb59813bc71
a18oscte <a18oscte@users.noreply.github.com>
2021-03-18 16:49:29 +0100

    Done with the GET respone
    Might work on the SELECT query  at line 29 later

--- PHP/RESTapi-MySQL/rest.php
+++ PHP/RESTapi-MySQL/rest.php
@@ -13,52 +13,13 @@ switch($_SERVER['REQUEST_METHOD'])
 {
 //If there is a get request
 case 'GET': $the_request = &$_GET;
-
-    $get_arr = array(
-        "id" =>  isset($_GET['id']) ? $_GET['id'] : null,
-        "airline" => isset($_GET['airline']) ? $_GET['airline'] : null,
-        "airlineId" => isset($_GET['airlineId']) ? $_GET['airlineId'] : null,
-        "sourceAirport" => isset($_GET['sourceAirport']) ? $_GET['sourceAirport'] : null,
-        "sourceAirportId" => isset($_GET['sourceAirportId']) ? $_GET['sourceAirportId'] : null,
-        "destinationAirport" => isset($_GET['destinationAirport']) ? $_GET['destinationAirport'] : null,
-        "destinationAirportId" => isset($_GET['destinationAirportId']) ? $_GET['destinationAirportId'] : null,
-        "stops" => isset($_GET['stops']) ? $_GET['stops'] : null,
-        "equipment" => isset($_GET['equipment']) ? $_GET['equipment'] : null
-
-    );
-    $query = "SELECT * FROM flightdata WHERE id LIKE :id OR airline LIKE :airline OR airlineId LIKE :airlineId OR sourceAirport LIKE :sourceAirport OR sourceAirportId LIKE :sourceAirportId OR destinationAirport LIKE :destinationAirport OR destinationAirportId LIKE :destinationAirportId OR stops LIKE :stops OR equipment LIKE :equipment";
-    $stmt = $conn->prepare($query);
-    $stmt->bindParam(":id", $get_arr['id']);
-    $stmt->bindParam(":airline", $get_arr['airline']);
-    $stmt->bindParam(":airlineId", $get_arr['airlineId']);
-    $stmt->bindParam(":sourceAirport", $get_arr['sourceAirport']);
-    $stmt->bindParam(":sourceAirportId", $get_arr['sourceAirportId']);
-    $stmt->bindParam(":destinationAirport", $get_arr['destinationAirport']);
-    $stmt->bindParam(":destinationAirportId", $get_arr['destinationAirportId']);
-    $stmt->bindParam(":stops", $get_arr['stops']);
-    $stmt->bindParam(":equipment", $get_arr['equipment']);
-    //$stmt = query($stmt);
-    $flightdata = array();
-    while ($row = $stmt->fetch()) {
-        $tmp = array("id" => $row['id']);
-        $tmp += ["airline" => $row['airline']];
-        $tmp += ["airlineId" => $row['airlineId']];
-        $tmp += ["sourceAirport" => $row['sourceAirport']];
-        $tmp += ["sourceAirportId" => $row['sourceAirportId']];
-        $tmp += ["destinationAirport" => $row['destinationAirport']];
-        $tmp += ["destinationAirportId" => $row['destinationAirportId']];
-        $tmp += ["stops" => $row['stops']];
-        $tmp += ["equipment" => $row['equipment']];
-        array_push($flightdata, $tmp);
-    }
-    echo json_encode($flightdata, JSON_PRETTY_PRINT);
-    /*
-    //Only works if there is data
-    if(!empty($data)){
-
-    }else{
+    //If there is no inparameters
+    if(empty($_GET)){
+        //Get all flightdatafrom the darabase
         $query = "SELECT * FROM flightdata";
         $stmt = $conn->query($query);
+       
+        //adding flightdata to a array
         $flightdata = array();
         while ($row = $stmt->fetch()) {
             $tmp = array("id" => $row['id']);
@@ -72,10 +33,58 @@ case 'GET': $the_request = &$_GET;
             $tmp += ["equipment" => $row['equipment']];
             array_push($flightdata, $tmp);
         }
+       
+        //responed with the array with all flightdata
         http_response_code(200);
         echo json_encode($flightdata, JSON_PRETTY_PRINT);
+    
+    //If there is inparameters
+    }else{
+        //Get the inparameters into an array
+        $get_arr = array(
+            "id" =>  isset($_GET['id']) ? $_GET['id'] : null,
+            "airline" => isset($_GET['airline']) ? $_GET['airline'] : null,
+            "airlineId" => isset($_GET['airlineId']) ? $_GET['airlineId'] : null,
+            "sourceAirport" => isset($_GET['sourceAirport']) ? $_GET['sourceAirport'] : null,
+            "sourceAirportId" => isset($_GET['sourceAirportId']) ? $_GET['sourceAirportId'] : null,
+            "destinationAirport" => isset($_GET['destinationAirport']) ? $_GET['destinationAirport'] : null,
+            "destinationAirportId" => isset($_GET['destinationAirportId']) ? $_GET['destinationAirportId'] : null,
+            "stops" => isset($_GET['stops']) ? $_GET['stops'] : null,
+            "equipment" => isset($_GET['equipment']) ? $_GET['equipment'] : null
+    
+        );
+        
+        //get the matching fligtdata from the database
+        $query = "SELECT * FROM flightdata WHERE id LIKE :id OR airline LIKE :airline OR airlineId LIKE :airlineId OR sourceAirport LIKE :sourceAirport OR sourceAirportId LIKE :sourceAirportId OR destinationAirport LIKE :destinationAirport OR destinationAirportId LIKE :destinationAirportId OR stops LIKE :stops OR equipment LIKE :equipment";
+        $stmt = $conn->prepare($query);
+        $stmt->execute(["id" => $get_arr['id'], "airline" => $get_arr['airline'], "airlineId" => $get_arr['airlineId'], "sourceAirport" => $get_arr['sourceAirport'], "sourceAirportId" => $get_arr['sourceAirportId'], "destinationAirport" => $get_arr['destinationAirport'], "destinationAirportId" => $get_arr['destinationAirportId'], "stops" => $get_arr['stops'], "equipment"=> $get_arr['equipment']]); 
+        
+        //adding flightdata to a array
+        $flightdata = array();
+        while ($row = $stmt->fetch()) {
+            $tmp = array("id" => $row['id']);
+            $tmp += ["airline" => $row['airline']];
+            $tmp += ["airlineId" => $row['airlineId']];
+            $tmp += ["sourceAirport" => $row['sourceAirport']];
+            $tmp += ["sourceAirportId" => $row['sourceAirportId']];
+            $tmp += ["destinationAirport" => $row['destinationAirport']];
+            $tmp += ["destinationAirportId" => $row['destinationAirportId']];
+            $tmp += ["stops" => $row['stops']];
+            $tmp += ["equipment" => $row['equipment']];
+            array_push($flightdata, $tmp);
+        }
+
+        //If there is matching flightdata in the database
+        if(!empty($flightdata)){
+            http_response_code(200);
+            //responed with the array with the flightdata
+            echo json_encode($flightdata, JSON_PRETTY_PRINT);
+        //If there is no matching flightdata in the database
+        }else{
+            http_response_code(404);
+            echo json_encode(array("message" => "No flightdata matched the get request"));
+        }
     }
-    */
     break;
 
 //If there is a post request
@@ -111,5 +120,6 @@ case 'POST': $the_request = &$_POST;
 
  // Etc.
 default:
+    http_response_code(503);
 }
 ?>
