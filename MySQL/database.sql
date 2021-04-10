824aeac11dc8627b88da7c62af0489f9d232a565
a18oscte <a18oscte@users.noreply.github.com>
2021-04-10 17:22:06 +0200

    Fixed script to post and get the time of every request

--- MySQL/database.sql
+++ MySQL/database.sql
@@ -4,12 +4,12 @@ USE examensarbete;
 
 CREATE TABLE flightdata (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-airline CHAR(3) NOT NULL,
-airlineId INT(6) NOT NULL,
-sourceAirport CHAR(4) NOT NULL,
-sourceAirportId INT(6) NOT NULL,
-destinationAirport CHAR(4) NOT NULL,
-destinationAirportId INT(6) NOT NULL,
-stops INT(6) NOT NULL,
-equipment VARCHAR(32) NOT NULL
+airline CHAR(3),
+airlineId INT(6),
+sourceAirport CHAR(4),
+sourceAirportId INT(6),
+destinationAirport CHAR(4),
+destinationAirportId INT(6),
+stops INT(6),
+equipment VARCHAR(32)
 ) ENGINE=InnoDB;
--- Scripts/post.js
+++ Scripts/post.js
@@ -1,30 +1,57 @@
 // ==UserScript==
-// @name         testare
+// @name         post
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://localhost/RESTapi-MySQL/
+// @match        http://localhost/
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-     $.getJSON("../flightdata.json", function(json) {
-          // this will show the info it in firebug console
-         for(var i = 0; i < json.length;i++){
-             console.log(i);
-             var xhr = new XMLHttpRequest();
-             xhr.open("POST", "http://localhost/RESTapi-MySQL/post.php", true);
-             xhr.setRequestHeader('Content-Type', 'application/json');
-             xhr.send(JSON.stringify(json[i]));
-             xhr.onreadystatechange = (e) => {
-                 console.log(xhr.responseText)
-             }
-         }
-         console.log("done");
-});
+    var iterations = 100;
+    var link = "http://localhost/PHP/RESTapi-MySQL/rest.php";
 
-    // Your code here...
+    var str="data:text/csv;charset=utf-8,";
+    var measurement;
+    var measurement2;
+
+    var btn = document.createElement("INPUT");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("value", "POST");
+    btn.onclick = function(){
+        $.getJSON("../flightdata.json", function(json) {
+            for(var i = 0; i < iterations;i++){
+                post(json[i]);
+                var delta = measurement2 - measurement;
+                str += delta + ",\n";
+                console.log(i);
+            }
+            console.log(str);
+            getData()
+        });
+    }
+    document.body.appendChild(btn);
+
+    function post(json){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", link, false);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        measurement = performance.now();
+        xhr.send(JSON.stringify(json));
+        measurement2 = performance.now();
+    }
+
+    function getData()
+    {
+        // Make anchor and click it!
+        var anchor = document.createElement("a");
+        anchor.setAttribute("href", encodeURI(str));
+        anchor.setAttribute("download", "my_data.csv");
+        anchor.innerHTML= "Click Here to download";
+        document.body.appendChild(anchor);
+        //anchor.click();
+    }
 })();
