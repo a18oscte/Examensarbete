ed8b86c8b86c9c665338f6ba6f2b2133460e924e
a18oscte <a18oscte@users.noreply.github.com>
2021-04-13 16:40:03 +0200

    Added comments to the scripts

--- Scripts/get.js
+++ Scripts/get.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Oscar Ternevid
 // @match        http://localhost/
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/seedrandom/3.0.5/seedrandom.min.js
@@ -13,19 +13,26 @@
 (function() {
     'use strict';
 
+    //Variables for use in measure the time of the request
     var str="data:text/csv;charset=utf-8,";
     var measurement;
     var measurement2;
 
+    //Variable used for choosing the link to the REST api
     var link;
 
-    var seedRandom = new Math.seedrandom('exjobb');
+    //Creates a object used to get seeded random numbers using the seedrandom.js library by David Bau
+    //Github link to the library: https://github.com/davidbau/seedrandom
+    var seededRandom = new Math.seedrandom('exjobb');
 
+    //Creating a div to hold the interface
     var div = document.createElement("DIV");
     div.setAttribute("id", "getbox");
     div.setAttribute("style", "border:1px solid black; display: inline-block;");
     document.body.appendChild(div);
 
+    //Creating a input for the different REST api:s
+    //PHP with MySQL
     var phpmysql = document.createElement("INPUT");
     phpmysql.setAttribute("type", "radio");
     phpmysql.setAttribute("name", "link");
@@ -35,6 +42,7 @@
     phpmysqltext.innerHTML= "PHP MySQL<br>";
     document.getElementById("getbox").appendChild(phpmysqltext);
 
+    //PHP without MySQL
     var phpnomysql = document.createElement("INPUT");
     phpnomysql.setAttribute("type", "radio");
     phpnomysql.setAttribute("name", "link");
@@ -44,6 +52,7 @@
     phpnomysqltext.innerHTML= "PHP without MySQL<br>";
     document.getElementById("getbox").appendChild(phpnomysqltext);
 
+    //Flask with MySQL
     var flaskmysql = document.createElement("INPUT");
     flaskmysql.setAttribute("type", "radio");
     flaskmysql.setAttribute("name", "link");
@@ -53,6 +62,7 @@
     flaskmysqltext.innerHTML= "FLASK MySQL<br>";
     document.getElementById("getbox").appendChild(flaskmysqltext);
 
+    //Flask without MySQL
     var flasknomysql = document.createElement("INPUT");
     flasknomysql.setAttribute("type", "radio");
     flasknomysql.setAttribute("name", "link");
@@ -62,22 +72,27 @@
     flasknomysqltext.innerHTML= "FLASK without MySQL<br>";
     document.getElementById("getbox").appendChild(flasknomysqltext);
 
+    //Creating a input for the amount of iterations
     var iterations;
     var iterationsinput = document.createElement("INPUT");
     iterationsinput.setAttribute("type", "number");
     document.getElementById("getbox").appendChild(iterationsinput);
 
+    //Creating a button to start the get requests
     var btn = document.createElement("INPUT");
     btn.setAttribute("type", "button");
     btn.setAttribute("value", "GET");
     btn.onclick = function(){
+        //Gets the amount of iterations and the link to the chosen REST api
         iterations = iterationsinput.value;
         link = document.querySelector('input[name="link"]:checked').value;
 
+        //Gets the JSON file with the filghtdata
         $.getJSON("../flightdata.json", function(json) {
             for(var i = 0; i < iterations;i++){
                 var param = randomParam(json);
                 get(param);
+                //calculates the time it took to send the request and get a response from the REST api
                 var delta = measurement2 - measurement;
                 str += delta + ",\n";
             }
@@ -86,21 +101,27 @@
     }
     document.getElementById("getbox").appendChild(btn);
 
+    //Creating a button to start the get all flightdata requests
     var getAllBtn = document.createElement("INPUT");
     getAllBtn.setAttribute("type", "button");
     getAllBtn.setAttribute("value", "GET ALL");
     getAllBtn.onclick = function(){
+        //Gets the amount of iterations and the link to the chosen REST api
         iterations = iterationsinput.value;
         link = document.querySelector('input[name="link"]:checked').value;
+
         for(var i = 0; i < iterations;i++){
-                getAll();
-                var delta = measurement2 - measurement;
-                str += delta + ",\n";
-            }
+            getAll();
+            //calculates the time it took to send the request and get a response from the REST api
+            var delta = measurement2 - measurement;
+            str += delta + ",\n";
+        }
         getData()
     }
     document.getElementById("getbox").appendChild(getAllBtn);
 
+    //Sends a HTTP request to the chosen REST api with the parameters
+    //Saves the time it takes to send he request and get a response from the REST api
     function get(param){
         var xhr = new XMLHttpRequest();
         xhr.open("GET", link + param, false);
@@ -111,6 +132,8 @@
         measurement2 = performance.now();
     }
 
+    //Sends a HTTP request to the chosen REST api without parameters to get all flightdata
+    //Saves the time it takes to send he request and get a response from the REST api
     function getAll(){
         var xhr = new XMLHttpRequest();
         xhr.open("GET", link, false);
@@ -121,6 +144,7 @@
         measurement2 = performance.now();
     }
 
+    //Creates a link to download a csv file with the data collected from the test
     function getData()
     {
         var anchor = document.createElement("a");
@@ -128,13 +152,15 @@
         anchor.setAttribute("download", "my_data.csv");
         anchor.innerHTML= "Click Here to download";
         document.body.appendChild(anchor);
-        //anchor.click();
     }
 
-    function randomParam(array) {
-        var obj = array[array.length * seedRandom() << 0];
+    //Gets a random key and value from the JSON file with the help from seedrandom.js
+    //Then returns a string with that can be added to the link to the REST api as a parameter
+    function randomParam(json) {
+        var obj = json[json.length * seededRandom() << 0];
         var keys = Object.keys(obj);
-        var key = keys[ keys.length * seedRandom() << 0];
+        var key = keys[ keys.length * seededRandom() << 0];
+
         return "?" + key + "=" + obj[key];
     };
 })();
--- Scripts/post.js
+++ Scripts/post.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Oscar Ternevid
 // @match        http://localhost/
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @grant        none
@@ -12,17 +12,22 @@
 (function() {
     'use strict';
 
+    //Variables for use in measure the time of the request
     var str="data:text/csv;charset=utf-8,";
     var measurement;
     var measurement2;
 
+    //Variable used for choosing the link to the REST api
     var link;
 
+    //Creating a div to hold the interface
     var div = document.createElement("DIV");
     div.setAttribute("id", "postbox");
     div.setAttribute("style", "border:1px solid black; display: inline-block;");
     document.body.appendChild(div);
 
+    //Creating a input for the different REST api:s
+    //PHP with MySQL
     var phpmysql = document.createElement("INPUT");
     phpmysql.setAttribute("type", "radio");
     phpmysql.setAttribute("name", "link");
@@ -32,6 +37,7 @@
     phpmysqltext.innerHTML= "PHP MySQL<br>";
     document.getElementById("postbox").appendChild(phpmysqltext);
 
+    //PHP without MySQL
     var phpnomysql = document.createElement("INPUT");
     phpnomysql.setAttribute("type", "radio");
     phpnomysql.setAttribute("name", "link");
@@ -41,6 +47,7 @@
     phpnomysqltext.innerHTML= "PHP without MySQL<br>";
     document.getElementById("postbox").appendChild(phpnomysqltext);
 
+    //Flask with MySQL
     var flaskmysql = document.createElement("INPUT");
     flaskmysql.setAttribute("type", "radio");
     flaskmysql.setAttribute("name", "link");
@@ -50,6 +57,7 @@
     flaskmysqltext.innerHTML= "FLASK MySQL<br>";
     document.getElementById("postbox").appendChild(flaskmysqltext);
 
+    //Flask without MySQL
     var flasknomysql = document.createElement("INPUT");
     flasknomysql.setAttribute("type", "radio");
     flasknomysql.setAttribute("name", "link");
@@ -59,21 +67,26 @@
     flasknomysqltext.innerHTML= "FLASK without MySQL<br>";
     document.getElementById("postbox").appendChild(flasknomysqltext);
 
+    //Creating a input for the amount of iterations
     var iterations;
     var iterationsinput = document.createElement("INPUT");
     iterationsinput.setAttribute("type", "number");
     document.getElementById("postbox").appendChild(iterationsinput);
 
+    //Creating a button to start the post requests
     var btn = document.createElement("INPUT");
     btn.setAttribute("type", "button");
     btn.setAttribute("value", "POST");
     btn.onclick = function(){
+        //Gets the amount of iterations and the link to the chosen REST api
         iterations = iterationsinput.value;
         link = document.querySelector('input[name="link"]:checked').value;
 
+        //Gets the JSON file with the filghtdata
         $.getJSON("../flightdata.json", function(json) {
             for(var i = 0; i < iterations;i++){
                 post(json[i]);
+                //calculates the time it took to send the request and get a response from the REST api
                 var delta = measurement2 - measurement;
                 str += delta + ",\n";
                 console.log(i);
@@ -83,6 +96,8 @@
     }
     document.getElementById("postbox").appendChild(btn);
 
+    //Sends a HTTP request to the chosen REST api with the JSON data from the file
+    //Saves the time it takes to send he request and get a response from the REST api
     function post(json){
         var xhr = new XMLHttpRequest();
         xhr.open("POST", link, false);
@@ -93,6 +108,7 @@
         measurement2 = performance.now();
     }
 
+    //Creates a link to download a csv file with the data collected from the test
     function getData()
     {
         var anchor = document.createElement("a");
@@ -100,6 +116,5 @@
         anchor.setAttribute("download", "my_data.csv");
         anchor.innerHTML= "Click Here to download";
         document.body.appendChild(anchor);
-        //anchor.click();
     }
 })();
