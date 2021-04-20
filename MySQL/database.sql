266c6868bdd1c2b8140d8873ee08163efef3b605
a18oscte <a18oscte@users.noreply.github.com>
2021-04-20 20:09:57 +0200

    made code prettier

--- MySQL/database.sql
+++ MySQL/database.sql
@@ -3,13 +3,13 @@ CREATE DATABASE examensarbete;
 USE examensarbete;
 
 CREATE TABLE flightdata (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-airline CHAR(3),
-airlineId INT(6),
-sourceAirport CHAR(4),
-sourceAirportId INT(6),
-destinationAirport CHAR(4),
-destinationAirportId INT(6),
-stops INT(6),
-equipment VARCHAR(32)
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	airline CHAR(3),
+	airlineId INT(6),
+	sourceAirport CHAR(4),
+	sourceAirportId INT(6),
+	destinationAirport CHAR(4),
+	destinationAirportId INT(6),
+	stops INT(6),
+	equipment VARCHAR(32)
 ) ENGINE=InnoDB;
